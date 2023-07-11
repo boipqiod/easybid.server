5ccd6e40483d50e7d22381132a8f1fa603ab8b5c
공상헌 <66378324+boipqiod@users.noreply.github.com>
2023-07-11 14:29:01 +0900

    TEST

--- app.ts
+++ app.ts
@@ -1,20 +1,16 @@
 import express from "express";
-import http from "http"
 import cors from 'cors'
 import router from "./src/routes/data";
 import authRouter from "./src/routes/auth";
 
 const app = express();
-const port = 8080;
+const port = 3003;
 
 app.use(express.json());
 app.use(cors());
 
-
 app.use('/data', router)
 app.use('/auth', authRouter)
-
-const server = http.createServer(app);
-server.listen(port, '0.0.0.0', function() {
-    console.log('Listening to port:  ' + port);
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
